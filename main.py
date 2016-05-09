792601aa7594c0a39b481a668ffe15f4bb87d583
Romain Claret <contact@rocla.ch>
2016-05-09 14:47:12 +0200

    / temperature value can be negative now

--- main.py
+++ main.py
@@ -15,11 +15,12 @@ if __name__ == '__main__':
 
     ###HSBDecode
     ##### yellow=(50,100,100), blue=(240,100,100), red=(0,100,100), white(0,0,100), black(0,0,0)
-    color = HSBColor(0,0,100) # (50,100,100) == temp: -0.05
+    ##### blue_cold=(176,0,0), blue_cold_dark(176,0,255)
+    color = HSBColor(0,0,255)
 
     print(color)
     print("temperature", color.temperature())
-    print("luminosite", color.brightness())  # luminosity is between 0 and 0,4
+    print("luminosite", color.brightness())
 
     ##FaceDetect
 
@@ -33,17 +34,15 @@ if __name__ == '__main__':
     #### Each analyse add 1 unit to the total
     tmp_type_of_analyse = 2  # temperature, brightness
 
-    tmp_temperature = abs(color.temperature()) + 0.001  # avoid absolute 0
-    tmp_max_luminosity = 0.4
-    tmp_luminosity = (abs(color.brightness()) + 0.001)*(1/tmp_max_luminosity)  # avoid absolute 0
+    tmp_temperature = color.temperature()
+    tmp_max_luminosity = 1
+    tmp_luminosity = color.brightness()*(1/tmp_max_luminosity)
     tmp_emotion_levels = len(emotion_levels)
     tmp_emotion_unit = 100 / tmp_emotion_levels
     tmp_periods_to_play = 10
 
-    #### Mood on a scale of 0..100 with 100:happy and 0:sad
-    ####
+    #### Mood on a scale of -100..100 with 100:happy and -100:sad
     #### Add up of positive values
-    ##### yellow=(50,100,100), blue=(240,100,100), red=(0,100,100), white(0,0,100), black(0,0,0)
     mood_value = (tmp_temperature + tmp_luminosity) / tmp_type_of_analyse * 100
     print(mood_value)
 
@@ -51,10 +50,14 @@ if __name__ == '__main__':
     #sentiment = (mood_value / tmp_emotion_unit)
     sentiment = 0
     for i in range(0, tmp_emotion_levels+1):
-        if mood_value <= i * tmp_emotion_unit:
+        if mood_value < 0 and abs(mood_value) <= i*tmp_emotion_unit:
             sentiment = abs(i-5)
+            print("neg")
             break
-    print(str(sentiment))
+        if mood_value >= 0 and mood_value <= i*tmp_emotion_unit:
+            sentiment = abs(i-5)
+            print("pos")
+            break
 
     #Play Music
     play_music(tmp_periods_to_play, sentiment)
