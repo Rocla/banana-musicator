b6f187d49e13445786e027347ad025a8101fcd10
Romain Claret <contact@rocla.ch>
2016-05-10 17:15:26 +0200

    + argument skeleton

--- main.py
+++ main.py
@@ -12,29 +12,34 @@ from HistogramAnalyzer import HistogramAnalyzer
 from imageElementDetect import ImageElementDetect
 from Orchestra import play_music
 
-if __name__ == '__main__':
 
-    #Verbose options
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        argument1 = int(sys.argv[1])
+        argument2 = int(sys.argv[2])
+        argument3 = int(sys.argv[3])
+
+    # Verbose options
     options_play_music = True
     options_verbose = True
 
-    #imagePath = path + '/images/abstract_blue_orange_red.jpg'
+    # imagePath = path + '/images/abstract_blue_orange_red.jpg'
     imagePath = path + '/images/watson.jpg'
 
-    #Extraction informations
+    # Extraction informations
     img = cv2.imread(imagePath)
     histo_tool = HistogramAnalyzer(img)
 
-    #print(histo_tool.get_hue_max())
-    #print(histo_tool.get_hue_average())
-    #print(histo_tool.get_saturation_max())
-    #print(histo_tool.get_brigthness_max())
+    # print(histo_tool.get_hue_max())
+    # print(histo_tool.get_hue_average())
+    # print(histo_tool.get_saturation_max())
+    # print(histo_tool.get_brigthness_max())
 
     ###HSBDecode
     ##### yellow=(50,100,100), blue=(240,100,100), red=(0,100,100), white(0,0,100), black(0,0,0)
     ##### blue_cold=(176,0,0), blue_cold_dark(176,0,255)
 
-    #color = HSBColor(0,0,255)
+    # color = HSBColor(0,0,255)
     color = HSBColor(histo_tool.get_hue_average(),
                      histo_tool.get_saturation_average(),
                      histo_tool.get_brigthness_average())
@@ -54,11 +59,11 @@ if __name__ == '__main__':
 
     ##FaceDetect
     detector = ImageElementDetect(imagePath, False)
-    #print("Nb faces : " + detector.countFaces())
-    #hasFaces = detector.hasFaces()
-    #hasFaces = False
+    # print("Nb faces : " + detector.countFaces())
+    # hasFaces = detector.hasFaces()
+    # hasFaces = False
 
-    #Get mood
+    # Get mood
     emotion_levels = ["HAPPY", "JAZZ", "EMO", "NEUTRAL", "SAD", "FACES"]
     ### Hot == temperature 1
     ### Cold == temperature 0
@@ -73,7 +78,7 @@ if __name__ == '__main__':
 
     tmp_temperature = color.temperature()
     tmp_max_luminosity = 1
-    tmp_luminosity = color.brightness()*(1/tmp_max_luminosity)
+    tmp_luminosity = color.brightness() * (1 / tmp_max_luminosity)
     tmp_emotion_levels = len(emotion_levels)
     tmp_emotion_unit = 100 / tmp_emotion_levels
     tmp_is_people = detector.hasFaces()
@@ -81,38 +86,39 @@ if __name__ == '__main__':
     #### Mood on a scale of -100..100 with 100:happy and -100:sad
     #### Add up of positive values
     mood_value = (tmp_temperature + tmp_luminosity) / tmp_type_of_analyse * 100
-    #print(mood_value)
+    # print(mood_value)
 
-    #Make music !
-    #sentiment = (mood_value / tmp_emotion_unit)
+    # Make music !
+    # sentiment = (mood_value / tmp_emotion_unit)
     sentiment = 0
-    for i in range(0, tmp_emotion_levels+1):
+    for i in range(0, tmp_emotion_levels + 1):
         if tmp_is_people:
             sentiment = 5
             if options_verbose:
                 print(emotion_levels[sentiment])
             break
-        elif mood_value < 0 and abs(mood_value) <= i*tmp_emotion_unit:
-            sentiment = abs(i-tmp_emotion_levels)
+        elif mood_value < 0 and abs(mood_value) <= i * tmp_emotion_unit:
+            sentiment = abs(i - tmp_emotion_levels)
             if options_verbose:
                 print(emotion_levels[i])
             break
-        elif mood_value >= 0 and mood_value <= i*tmp_emotion_unit:
-            sentiment = abs(i-tmp_emotion_levels)
+        elif mood_value >= 0 and mood_value <= i * tmp_emotion_unit:
+            sentiment = abs(i - tmp_emotion_levels)
             if options_verbose:
                 print(emotion_levels[i])
             break
 
-    #Show Image
+    # Show Image
 
     cv2.imshow('press_on_any_keyboard_key_to_play_the_music', img)
     cv2.waitKey(0)
 
-    #Play Music
+    # Play Music
     if options_play_music:
         play_music(tmp_periods_to_play, sentiment)
 
-    #Ending images
+    # Ending images
     cv2.destroyAllWindows()
 
     exit(0)
+
